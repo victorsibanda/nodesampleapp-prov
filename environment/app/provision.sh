0d2846fb06f7d9d676183ac66104b8c01c60a043
Victor Sibanda <victormsibanda1@gmail.com>
2020-03-25 10:08:05 +0000

    Java and Jenkins Added to provision

--- environment/app/provision.sh
+++ environment/app/provision.sh
@@ -26,3 +26,36 @@ sudo cp /home/ubuntu/environment/nginx.default /etc/nginx/sites-available/defaul
 
 # finally, restart the nginx service so the new config takes hold
 sudo service nginx restart
+
+# install Java
+sudo apt-get install openjdk-8-jdk openjdk-8-jre -y
+
+
+
+
+
+# sort out env variables
+cd /etc
+sudo chmod 777 environment
+cat >> /etc/environment <<EOL
+JAVA_HOME= /usr/lib/jvm/java-8-openjdk-amd64
+JRE_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre
+EOL
+sudo chmod 644 environment
+
+
+
+
+
+# Install jenkins
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+
+
+
+
+
+# print admin password
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
